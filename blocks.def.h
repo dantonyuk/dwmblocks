391b436627bf2207cdd1e79b277f35a8df006e04
Dmitry Antonyuk <32649457+dantonyuk@users.noreply.github.com>
2021-11-18 16:04:48 -0600

    Set up dwmblocks

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"", "~/.config/dwm/baritem upgrades",		60 * 60,		0},
+	{"", "~/.config/dwm/baritem cpu",			1,		0},
+	{"", "~/.config/dwm/baritem vol",			1,		10},
+	{"", "~/.config/dwm/baritem charge",		1,		0},
+	{"", "~/.config/dwm/baritem kbd_layout",	1,		11},
+	{"", "~/.config/dwm/baritem datetime",		1,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = "  ";
 static unsigned int delimLen = 5;
